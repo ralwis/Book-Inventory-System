9b0bc563c45c2d86c1cec2e02eac6a3c63e1a2a7
ravindu <ravindurcg99@yahoo.com>
2023-10-20 20:13:13 +0530

    enable CORS

--- Program.cs
+++ Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseCors(c => c.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
